70e950384bbe19d948d57551209ae00d1dae2e90
Steven Lembark <lembark@wrkhors.com>
2017-08-17 20:55:11 -0500

    auto-exit with status of child, log exits verbosely

--- lib/Docker/Reaper.pm
+++ lib/Docker/Reaper.pm
@@ -6,7 +6,9 @@ package Docker::Reaper v0.1.0;
 use v5.24;
 
 use Config;
-use Symbol  qw( qualify_to_ref );
+
+use Carp    qw( croak           );
+use Symbol  qw( qualify_to_ref  );
 
 ########################################################################
 # package variables
@@ -21,7 +23,7 @@ my $child           = '';
 my @sigz
 = grep
 {
-    index $_, 'CHLD' 
+    'CHLD' ne $_
 }
 split $Config{ sig_name };
 
@@ -37,6 +39,33 @@ my $propagate
     ? $verbose && say 'Signal propagated' 
     : warn "Failed propagate: '$sig' to $child\n"
     ;
+
+    return
+};
+
+########################################################################
+# utility subs
+########################################################################
+
+my $format_exit
+= sub
+{
+    my ( $pid, $status ) = @_;
+
+    my $exit    = $status >> 8;
+    my $core    = $status &  128;
+    my $signum  = $status & 0x0F;
+
+    my $message = "PID $pid exit( $exit )";
+    $message    .= ' with core'         if $core;
+    $message    .= " killed by $signum" if $signum;
+
+    $message
+};
+
+my $log_exit_stack
+= sub
+{
 
     return
 };
@@ -63,7 +92,9 @@ sub init
     {
         @_
         ? exec @_
-        : exec @ARGV
+        : @ARGV
+        ? exec @ARGV
+        : croak 'Bogus init: both @_ and @ARGV are empty.'
         ;
 
         die "Failed exec: $!\n"
@@ -72,7 +103,7 @@ sub init
     # child never gets this far in the code via exec or die, above.
     #
     # parent waits for the child to exit, propagating any 
-    # signals that can be and reaping any children.
+    # trap-able signals and reaping any children.
 
     local @SIG{ @sigz } = ( $propagate ) x @sigz;
 
@@ -93,10 +124,18 @@ sub init
 
             push @exitz, [ $pid, $? ]
             if $log_exits;
+
+            next
         };
 
         say "Child exit: $? ($child)";
         push @exitz, [ $child, $? ];
+
+        if( $verbose )
+        {
+            say $format_exit->( @$_ )
+            for @exitz;
+        }
 
         last
     }
@@ -123,6 +162,24 @@ sub import
 
         $log_exits  = $arg // '';
     }
+
+    my $init_exits
+    = do
+    {
+        if
+        (
+            my ( $found ) = grep { ! index $_, ':exit=' } @_ 
+        )
+        {
+            my ( undef, $arg ) = split '=' => $found, 2;
+
+            $arg // ''
+        }
+        else
+        {
+            ''
+        }
+    };
     
     if
     (
@@ -139,12 +196,15 @@ sub import
         my $exitz
         = $arg
         ? init $arg
-        : init @ARGV
+        : @ARGV
+        ? init @ARGV
+        : croak 'Bogus Docker::Reaper: both ":exec=" argument & @ARGV empty';
         ;
 
         # i.e., propagate the child's exit value.
 
-        exit $exitz->[-1][-1] >> 8;
+        exit $exitz->[-1][-1] >> 8
+        if $init_exits;
     }
     else
     {
@@ -159,3 +219,58 @@ sub import
 # keep require happy
 1
 __END__
+
+=head1 NAME
+
+Docker::Reaper - Init-ish function to reap children.
+
+=head1 SYNOSIS
+
+    # import 'init' sub
+    # perform any setup.
+    # run init to fork-exec the program with arguments, wait
+    # for it to exit, reaping defunct proc's. this will also
+    # propagate any trapable signals to the child process.
+
+    use Docker::Reaper;
+
+    init qw( /path/to/program arg arg ... );
+
+    # immediately exec the program at BEGIN time when the 
+    # module is used, optionally with arguments.
+    #
+    # note that this will fork-exec the program from a call
+    # in Docker::Reaper::import, the use will *not* return 
+    # until the child process has exited. this basically turns
+    # the remainder of any code into cleanup for the exec'd
+    # program.
+
+    use Docker::Reaper qw( :exec=/path/to/program );
+
+    use Docker::Reaper ':exec=/path/to/program arg arg ...';
+
+    # false value to exec uses @ARGV.
+
+    use Docker::Reaper qw( :exec= );
+
+
+    # turn on logging of exit status to stdout for all exiting
+    # procs, not just the child.
+
+    use Docker::Reaper qw( ":log_exits=1" );
+
+    # turn off loggin of exits even if INIT_LOG_EXITS is set
+    # in the environment.
+
+    use Docker::Reaper qw( ":log_exits=0" );
+
+
+    # turn on verbose logging in init, this sets INIT_LOG_EXITS.
+
+    export VERBOSE=1;
+
+    # turn on logging of exits.
+
+    export INIT_LOG_EXITS=1;
+
+
